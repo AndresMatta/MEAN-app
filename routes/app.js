b34e030262292277c9ce94784460b0b3431cf579
root <andres.sm123@gmail.com>
2017-06-05 15:47:46 -0600

    Store and Fetch data

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+	User.findOne({}, function(error, doc){
+		if (error) {
+			return res.send('Error mier');
+		}
+
+		res.render('node', { email: doc.email });
+	});
+});
+
+router.post('/', function(req, res, next) {
+	var email = req.body.email;
+	var user = new User({
+		firstName: 'Andrés',
+		lastName: 'Matta',
+		password: 'contraseña',
+		email: email
+	});
+
+	user.save();
+	res.redirect('/');
 });
 
 module.exports = router;
